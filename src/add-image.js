703ce31d5de2fe92139019ee6959146b890d82a1
akhiltheguitarist <akhiltheguitarist@gmail.com>
2020-07-24 16:05:46 +0530

    Initial commit files

---
+++ src/add-image.js
@@ -0,0 +1,13 @@
+import * as Sunrise from './sunrise.png';
+
+function addImage() {
+    const img = document.createElement('img');
+    img.alt = 'sunrise';
+    img.width = 300;
+    img.src = Sunrise;
+
+    const body = document.querySelector('body');
+    body.appendChild(img);
+}
+
+export default addImage;
